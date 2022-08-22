05161d0024a4a9ecdb027dcf6c7539544e40832b
Adam Robinson <30032798+adamistheanswer@users.noreply.github.com>
2022-08-22 18:15:35 +0100

    init3

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,64 +8,64 @@
 // `npm start            (this starts nodejs with express and serves the ./dist/client folder)
 // visit http://127.0.0.1:3000
 
-import express from 'express'
-import path from 'path'
-import http from 'http'
-import { Server, Socket } from 'socket.io'
+import express from "express";
+import path from "path";
+import http from "http";
+import { Server, Socket } from "socket.io";
 
-const port: number = 3000
+const port: number | string = process.env.PORT || 5000;
 
 class App {
-    private server: http.Server
-    private port: number
+  private server: http.Server;
+  private port: number;
 
-    private io: Server
-    private clients: any = {}
+  private io: Server;
+  private clients: any = {};
 
-    constructor(port: number) {
-        this.port = port
-        const app = express()
-        app.use(express.static(path.join(__dirname, '../client')))
+  constructor(port: number) {
+    this.port = port;
+    const app = express();
+    app.use(express.static(path.join(__dirname, "../client")));
 
-        this.server = new http.Server(app)
+    this.server = new http.Server(app);
 
-        this.io = new Server(this.server)
+    this.io = new Server(this.server);
 
-        this.io.on('connection', (socket: Socket) => {
-            console.log(socket.constructor.name)
-            this.clients[socket.id] = {}
-            console.log(this.clients)
-            console.log('a user connected : ' + socket.id)
-            socket.emit('id', socket.id)
+    this.io.on("connection", (socket: Socket) => {
+      console.log(socket.constructor.name);
+      this.clients[socket.id] = {};
+      console.log(this.clients);
+      console.log("a user connected : " + socket.id);
+      socket.emit("id", socket.id);
 
-            socket.on('disconnect', () => {
-                console.log('socket disconnected : ' + socket.id)
-                if (this.clients && this.clients[socket.id]) {
-                    console.log('deleting ' + socket.id)
-                    delete this.clients[socket.id]
-                    this.io.emit('removeClient', socket.id)
-                }
-            })
+      socket.on("disconnect", () => {
+        console.log("socket disconnected : " + socket.id);
+        if (this.clients && this.clients[socket.id]) {
+          console.log("deleting " + socket.id);
+          delete this.clients[socket.id];
+          this.io.emit("removeClient", socket.id);
+        }
+      });
 
-            socket.on('update', (message: any) => {
-                if (this.clients[socket.id]) {
-                    this.clients[socket.id].t = message.t //client timestamp
-                    this.clients[socket.id].p = message.p //position
-                    this.clients[socket.id].r = message.r //rotation
-                }
-            })
-        })
+      socket.on("update", (message: any) => {
+        if (this.clients[socket.id]) {
+          this.clients[socket.id].t = message.t; //client timestamp
+          this.clients[socket.id].p = message.p; //position
+          this.clients[socket.id].r = message.r; //rotation
+        }
+      });
+    });
 
-        setInterval(() => {
-            this.io.emit('clients', this.clients)
-        }, 50)
-    }
+    setInterval(() => {
+      this.io.emit("clients", this.clients);
+    }, 50);
+  }
 
-    public Start() {
-        this.server.listen(this.port, () => {
-            console.log(`Server listening on port ${this.port}.`)
-        })
-    }
+  public Start() {
+    this.server.listen(this.port, () => {
+      console.log(`Server listening on port ${this.port}.`);
+    });
+  }
 }
 
-new App(port).Start()
+new App(port).Start();
