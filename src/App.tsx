cebdb2be9d49ad309bc26db9be2a6b5509f5f1c3
Adam Robinson <30032798+adamistheanswer@users.noreply.github.com>
2022-09-13 20:59:33 +0100

    Collision Detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Canvas, extend } from '@react-three/fiber'
 import { Stats } from '@react-three/drei'
 import { io, Socket } from 'socket.io-client'
 import UserWrapper from './UserWrapper'
-import { AmbientLight, SpotLight, PointLight, GridHelper } from 'three'
+import {
+    AmbientLight,
+    SpotLight,
+    PointLight,
+    GridHelper,
+    Mesh,
+    BoxGeometry,
+} from 'three'
 import ControlsWrapper from './ControlsWrapper'
 
 extend({
@@ -17,6 +24,8 @@ function App() {
     const [clientSocket, setSocketClient] = useState<Socket | null>(null)
     const [clients, setClients] = useState({})
 
+    const remoteColliders = useRef<any>([])
+
     useEffect(() => {
         setSocketClient(io())
 
@@ -24,6 +33,31 @@ function App() {
             if (clientSocket) clientSocket.disconnect()
         }
     }, [])
+
+    useEffect(() => {
+        if (clientSocket) {
+            let cols = []
+            Object.keys(clients)
+                .filter((clientKey) => clientKey !== clientSocket.id)
+                .map((client) => {
+                    const { position, rotation } = clients[client]
+                    const geometry = new BoxGeometry(1, 1, 1)
+
+                    let player = new Mesh(geometry)
+                    player.position.set(position[0], position[1], position[2])
+                    player.rotation.set(
+                        rotation[0],
+                        rotation[1],
+                        rotation[2],
+                        'XYZ'
+                    )
+                    player.updateMatrixWorld()
+                    //@ts-ignore
+                    cols.push(player)
+                })
+            remoteColliders.current = cols
+        }
+    }, [clients])
 
     useEffect(() => {
         if (clientSocket) {
@@ -41,7 +75,10 @@ function App() {
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
                 <pointLight position={[-10, -10, -10]} />
                 <gridHelper position-y={-0.5} />
-                <ControlsWrapper clientSocket={clientSocket} />
+                <ControlsWrapper
+                    clientSocket={clientSocket}
+                    remoteColliders={remoteColliders}
+                />
                 {Object.keys(clients)
                     .filter((clientKey) => clientKey !== clientSocket.id)
                     .map((client) => {
