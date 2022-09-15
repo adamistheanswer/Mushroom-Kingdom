3398ee129dd9f519789e689527097b10276ce36b
Adam Robinson <30032798+adamistheanswer@users.noreply.github.com>
2022-09-16 00:25:56 +0100

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -6,94 +6,94 @@ import parser from 'socket.io-msgpack-parser'
 import UserWrapper from './UserWrapper'
 
 import {
-    AmbientLight,
-    SpotLight,
-    PointLight,
-    GridHelper,
-    Mesh,
-    BoxGeometry,
+   AmbientLight,
+   SpotLight,
+   PointLight,
+   GridHelper,
+   Mesh,
+   BoxGeometry,
 } from 'three'
 import ControlsWrapper from './ControlsWrapper'
 
 extend({
-    AmbientLight,
-    SpotLight,
-    PointLight,
-    GridHelper,
+   AmbientLight,
+   SpotLight,
+   PointLight,
+   GridHelper,
 })
 
 function App() {
-    const [clientSocket, setSocketClient] = useState<Socket | null>(null)
-    const [clients, setClients] = useState({})
+   const [clientSocket, setSocketClient] = useState<Socket | null>(null)
+   const [clients, setClients] = useState({})
 
-    const boxGemo = useMemo(() => new BoxGeometry(1, 1, 1), [])
-    const mesh = useMemo(() => new Mesh(boxGemo), [boxGemo])
+   const boxGemo = useMemo(() => new BoxGeometry(1, 1, 1), [])
+   const mesh = useMemo(() => new Mesh(boxGemo), [boxGemo])
 
-    const remoteColliders = useRef<any>([])
+   const remoteColliders = useRef<any>([])
 
-    useEffect(() => {
-        setSocketClient(io({ parser }))
+   useEffect(() => {
+      setSocketClient(io({ parser }))
 
-        return () => {
-            if (clientSocket) clientSocket.disconnect()
-        }
-    }, [])
+      return () => {
+         if (clientSocket) clientSocket.disconnect()
+      }
+   }, [])
 
-    useEffect(() => {
-        if (clientSocket) {
-            let cols = []
-            Object.keys(clients)
-                .filter((clientKey) => clientKey !== clientSocket.id)
-                .map((client) => {
-                    const { p, r } = clients[client]
+   useEffect(() => {
+      if (clientSocket) {
+         let cols = []
+         Object.keys(clients)
+            .filter((clientKey) => clientKey !== clientSocket.id)
+            .map((client) => {
+               const { p, r } = clients[client]
 
-                    let player = mesh
-                    player.position.set(p[0], p[1], p[2])
-                    player.rotation.set(0, r, 0, 'XYZ')
-                    player.updateMatrixWorld()
-                    //@ts-ignore
-                    cols.push(player)
-                })
-            remoteColliders.current = cols
-        }
-    }, [clients])
-
-    useEffect(() => {
-        if (clientSocket) {
-            clientSocket.on('clientUpdates', (updatedClients) => {
-                setClients(updatedClients)
+               let player = mesh
+               player.position.set(p[0], p[1], p[2])
+               player.rotation.set(0, r, 0, 'XYZ')
+               player.updateMatrixWorld()
+               //@ts-ignore
+               cols.push(player)
             })
-        }
-    }, [clientSocket])
+         remoteColliders.current = cols
+      }
+   }, [clients])
 
-    return (
-        clientSocket && (
-            <Canvas camera={{ position: [0, 4, 4] }}>
-                <Stats />
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                <pointLight position={[-10, -10, -10]} />
-                <gridHelper position-y={-0.5} />
-                <ControlsWrapper
-                    clientSocket={clientSocket}
-                    remoteColliders={remoteColliders}
-                />
-                {Object.keys(clients)
-                    .filter((clientKey) => clientKey !== clientSocket.id)
-                    .map((client) => {
-                        const { p, r } = clients[client]
-                        return (
-                            <UserWrapper
-                                key={client}
-                                id={client}
-                                position={p}
-                                rotation={r}
-                            />
-                        )
-                    })}
-            </Canvas>
-        )
-    )
+   useEffect(() => {
+      if (clientSocket) {
+         clientSocket.on('clientUpdates', (updatedClients) => {
+            setClients(updatedClients)
+         })
+      }
+   }, [clientSocket])
+
+   return (
+      clientSocket && (
+         <Canvas camera={{ position: [0, 4, 4] }}>
+            <Stats />
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <gridHelper position-y={-0.5} />
+            <ControlsWrapper
+               clientSocket={clientSocket}
+               remoteColliders={remoteColliders}
+            />
+            {Object.keys(clients)
+               .filter((clientKey) => clientKey !== clientSocket.id)
+               .map((client) => {
+                  const { p, r } = clients[client]
+                  return (
+                     <UserWrapper
+                        key={client}
+                        id={client}
+                        position={p}
+                        rotation={r}
+                     />
+                  )
+               })}
+         </Canvas>
+      )
+   )
 }
 
 export default App
